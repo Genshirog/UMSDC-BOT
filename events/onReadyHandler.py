b7fee7320d55076b224b146e2867ff8063f5c19f
Genshirog <imbaxgx1fromyt@gmail.com>
2025-07-21 21:51:15 +0800

    update token

--- events/onReadyHandler.py
+++ events/onReadyHandler.py
@@ -7,14 +7,14 @@ async def on_ready_handler(bot: discord.Client):
     guild = discord.Object(id=1392045881695408158)
     #bot.tree.clear_commands(guild=guild)
     await bot.tree.sync(guild=guild)
-    try:
-        with open("verify_message.json","r") as f:
-            data = json.load(f)
-        guild = bot.get_guild(data["guild_id"])
-        channel = bot.get_channel(data["channel_id"])
-        message = await channel.fetch_message(data["message_id"])
-        view = buttonHandler(bot, title=data["title"], description=data["description"])
-        bot.add_view(view)
-        await message.edit(view=view)
-    except Exception as e:
-        print(f"❌ Could not restore verification message: {e}")
+    # try:
+    #     with open("verify_message.json","r") as f:
+    #         data = json.load(f)
+    #     guild = bot.get_guild(data["guild_id"])
+    #     channel = bot.get_channel(data["channel_id"])
+    #     message = await channel.fetch_message(data["message_id"])
+    #     view = buttonHandler(bot, title=data["title"], description=data["description"])
+    #     bot.add_view(view)
+    #     await message.edit(view=view)
+    # except Exception as e:
+    #     print(f"❌ Could not restore verification message: {e}")
